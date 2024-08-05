cafae5cb0fa66198fbdf3d7fd898e5acc2b636ae
rsgm1978 <159719756+rsgm1978@users.noreply.github.com>
2024-08-05 18:16:36 +0000

    maetodomap

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+let pronun = ['the','our'];
+let adj = ['great', 'big'];
+let noun = [ 'jogger', 'racoon'];
+let  TextForMap = ''
+pronun((elementPronuon) =>{
+  adj.map((elementAdj) =>{
+    noun.map((elementNounNoun) =>{
+      TextForMap +=
+      <li class='list-group-item'>
+        ${elementPronuon}${elementAdj}${elementNoun}.es
+      </li>
+    })
+  })
+}
+)
+
+
+document.querySelector (#for-map).innerHTML = TextForMap
   console.log("Hello Rigo from the console!");
 };
